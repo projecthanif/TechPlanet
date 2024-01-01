44956c1eb107fb87ca9ff8ab65ce138cd9a3a321
projecthanif <iamustapha213@gmail.com>
2024-01-01 07:16:43 +0100

    made it responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,11 +3,18 @@
 function navToggle() {
     const navBar = document.querySelector('ul.navbar');
     const navToggle = document.querySelector('.nav-toggle')
+    const font = document.querySelector('i.fa.fa-bars.fa-1x');
 
     navToggle.addEventListener('click', () => {
         navBar.className = 'showNav';
-    })
+        font.className = 'fa fa-plus fa-1x';
+    });
+
+    // font.addEventListener('click', () => {
+    //     navBar.className = 'navbar';
+    //     font.className = 'fa fa-bars fa-1x';
+    // });
 
 }
 
-// navToggle();
+navToggle();
